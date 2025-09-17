29ff0cc0c6f7112c576ff6e5dca27f78f7f0fc40
Alex Salapanov <salapanov06@mail.ru>
2025-09-18 05:34:18 +0700

    Created repozitory

---
+++ 1Block/bubble_sort.py
@@ -0,0 +1,16 @@
+import argparse
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+parser = argparse.ArgumentParser()
+parser.add_argument("numbers", nargs="+", type=float)
+args = parser.parse_args()
+
+print("Исходный список:", args.numbers)
+print("Отсортированный список:", bubble_sort(args.numbers))
---
+++ 1Block/pascal_triangle.py
@@ -0,0 +1,23 @@
+import argparse
+
+def pascal(n):
+    triangle = [[1]]  # первая строка
+
+    for i in range(1, n):
+        prev = triangle[-1]
+        row = [1]
+        for j in range(len(prev) - 1):
+            row.append(prev[j] + prev[j + 1])
+        row.append(1)
+        triangle.append(row)
+
+    width = len(" ".join(map(str, triangle[-1])))
+    for row in triangle:
+        line = " ".join(map(str, row))
+        print(line.center(width))
+
+parser = argparse.ArgumentParser()
+parser.add_argument("rows", type=int,)
+args = parser.parse_args()
+
+pascal(args.rows)
