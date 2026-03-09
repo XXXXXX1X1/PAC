d2485e2db5acce809e5384e86decb67522ce2f15
xxx <a.salapanov@g.nsu.ru>
2026-03-09 20:43:57 +0700

    4_Lab 5_Lab

--- 4_semestr/4_Lab/download_fashionmnist.py
+++
@@ -1,6 +0,0 @@
-from torchvision import datasets
-
-datasets.FashionMNIST(root="./data", train=True, download=True)
-datasets.FashionMNIST(root="./data", train=False, download=True)
-
-print("Done. Saved to ./data")
